bdf0f9807b8987962df51404f9cc934aecaba93d
Joselyn Huaman <j.devetrieva@gmail.com>
2026-02-26 15:34:34 -0500

    actualizacion

--- proyecto_fase3_cusihuallpaJoselyn.py
+++ proyecto_fase3_cusihuallpaJoselyn.py
@@ -17,7 +17,7 @@ def iniciar_sesion(credenciales, maximo_intentos):
         if intentos < maximo_intentos: #Verifica si aún quedan intentos disponibles.
             print("Usuario o contraseña incorrecta. Intenta nuevamente para hacer tu pedido.") 
             # Si intentos == maximo_intentos, salimos del bucle y el else de la linea 120 se ejecuta.
-    return False
+    return False 
 
 # menu
 def tomar_pedido(menu, lista_productos):
